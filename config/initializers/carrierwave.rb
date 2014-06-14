931dcc3b30de1bf6ba7c77c9066dd0027cf7c304
Chris Lind <clind714@gmail.com>
2014-06-13 17:23:36 -0700

    uploads to s3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,13 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',       # required
-    :aws_access_key_id      => 'ENV["s3_key"]',       # required
-    :aws_secret_access_key  => 'ENV["s3_sec_key"]',        # required
-    :region                 => 'eu-west-1'  # optional, defaults to 'us-east-1'
+    :aws_access_key_id      => ENV['S3_KEY'],       # required
+    :aws_secret_access_key  => ENV['S3_SEC_KEY'],        # required
+    :region                 => 'us-east-1'  # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = 'ENV["s3_bucket"]'  # required
+  config.fog_directory  = ENV['S3_BUCKET']  # required
   # see https://github.com/jnicklas/carrierwave#using-amazon-s3
   # for more optional configuration
 end
+
+
